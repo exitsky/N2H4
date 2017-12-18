952b11b22e9a0297b428648cc1f95528c1581f69
ChanYub.Park <mrchypark@gmail.com>
2017-12-18 04:55:03 +0000

    add get summary func

--- R/getContent.R
+++ R/getContent.R
@@ -18,87 +18,110 @@
 #' @import rvest
 #' @import stringi
 
-getContent <- function(url, col = c("url", "datetime", "press", "title", "body"),
-                       try_cnt = 3, sleep_time = rnorm(1), async = FALSE, ...) {
-
-  if(!identical(url,character(0))){
-
-    tryn<-0
-    ua<-user_agent("N2H4 by chanyub.park <mrchypark@gmail.com>")
-    root<-try(httr::GET(url, ua), silent=T)
-    while(tryn<=try_cnt&&class(root)=="try-error"){
-      root<-try(httr::GET(url,ua), silent=T)
-      Sys.sleep(abs(rnorm(1)))
-      tryn<-tryn+1
-      print(paste0("try again: ", url))
-    }
-    urlcheck<-root$url
-
-    if(!identical(grep("^http://(news|finance).naver.com",urlcheck),integer(0))){
-
-      chk<-read_html(root, encoding="EUC-KR")%>%
-        html_nodes("div#main_content div div")%>%
-        html_attr("class") %>%.[1]
-
-      if(is.na(chk)){
-        chk<-"not error"
+getContent <-
+  function(url,
+           col = c("url", "datetime", "press", "title", "body"),
+           try_cnt = 3,
+           sleep_time = rnorm(1),
+           async = FALSE,
+           ...) {
+    if (!identical(url, character(0))) {
+      tryn <- 0
+      ua <- user_agent("N2H4 by chanyub.park <mrchypark@gmail.com>")
+      root <- try(httr::GET(url, ua), silent = T)
+      while (tryn <= try_cnt && class(root) == "try-error") {
+        root <- try(httr::GET(url, ua), silent = T)
+        Sys.sleep(abs(rnorm(1)))
+        tryn <- tryn + 1
+        print(paste0("try again: ", url))
       }
+      urlcheck <- root$url
 
-        if (RCurl::url.exists(url)&
-       "error_msg 404"!=chk
-        ) {
-          html_obj <- read_html(root, encoding="EUC-KR")
-          if(tryn>try_cnt){
-            newsInfo <- data.frame(url = url, datetime = "try out.",
-                                   edittime = "try out.",
-                                   press = "try out.",
-                                   title = "try out.",
-                                   body = "try out.",
-                                   stringsAsFactors = F)
-            return(newsInfo[,col])
+      if (!identical(grep("^http://(news|finance).naver.com", urlcheck),
+                     integer(0))) {
+        chk <- read_html(root, encoding = "EUC-KR") %>%
+          html_nodes("div#main_content div div") %>%
+          html_attr("class")
+        chk <-  chk[1]
+
+        if (is.na(chk)) {
+          chk <- "not error"
+        }
+
+        if (RCurl::url.exists(url) &
+            "error_msg 404" != chk) {
+          html_obj <- read_html(root, encoding = "EUC-KR")
+          if (tryn > try_cnt) {
+            newsInfo <- data.frame(
+              url = url,
+              datetime = "try out.",
+              edittime = "try out.",
+              press = "try out.",
+              title = "try out.",
+              body = "try out.",
+              stringsAsFactors = F
+            )
+            return(newsInfo[, col])
           }
-          title<-getContentTitle(html_obj)
-          datetime<-getContentDatetime(html_obj)[1]
-          edittime<-getContentDatetime(html_obj)[2]
-          press<-getContentPress(html_obj)
-          body<-getContentBody(html_obj)
+          title <- getContentTitle(html_obj)
+          datetime <- getContentDatetime(html_obj)[1]
+          edittime <- getContentDatetime(html_obj)[2]
+          press <- getContentPress(html_obj)
+          body <- getContentBody(html_obj)
 
-          newsInfo <- data.frame(url = url, datetime = datetime, edittime = edittime, press = press, title = title, body = body, stringsAsFactors = F)
+          newsInfo <-
+            data.frame(
+              url = url,
+              datetime = datetime,
+              edittime = edittime,
+              press = press,
+              title = title,
+              body = body,
+              stringsAsFactors = F
+            )
 
+        } else {
+          newsInfo <- data.frame(
+            url = url,
+            datetime = "page is moved.",
+            edittime = "page is moved.",
+            press = "page is moved.",
+            title = "page is moved.",
+            body = "page is moved.",
+            stringsAsFactors = F
+          )
+
+        }
+        return(newsInfo[, col])
       } else {
-
-        newsInfo <- data.frame(url = url, datetime = "page is moved.",
-                               edittime = "page is moved.",
-                               press = "page is moved.",
-                               title = "page is moved.",
-                               body = "page is moved.",
-                               stringsAsFactors = F)
+        newsInfo <-
+          data.frame(
+            url = url,
+            datetime = "page is not news section.",
+            edittime = "page is not news section.",
+            press = "page is not news section.",
+            title = "page is not news section.",
+            body = "page is not news section.",
+            stringsAsFactors = F
+          )
 
       }
-      return(newsInfo[,col])
+      return(newsInfo[, col])
     } else {
+      print("no news links")
 
-      newsInfo <- data.frame(url = url, datetime = "page is not news section.",
-                             edittime = "page is not news section.",
-                             press = "page is not news section.",
-                             title = "page is not news section.",
-                             body = "page is not news section.",
-                             stringsAsFactors = F)
-
+      newsInfo <- data.frame(
+        url = "no news links",
+        datetime = "no news links",
+        edittime = "no news links",
+        press = "no news links",
+        title = "no news links",
+        body = "no news links",
+        stringsAsFactors = F
+      )
+      return(newsInfo[, col])
     }
-    return(newsInfo[,col])
-  } else { print("no news links")
-
-    newsInfo <- data.frame(url = "no news links",
-                           datetime = "no news links",
-                           edittime = "no news links",
-                           press = "no news links",
-                           title = "no news links",
-                           body = "no news links",
-                           stringsAsFactors = F)
-    return(newsInfo[,col])
   }
-}
 
 
 
@@ -114,15 +137,22 @@ getContent <- function(url, col = c("url", "datetime", "press", "title", "body")
 #' @import xml2
 #' @import rvest
 
-getContentTitle<-function(html_obj, title_node_info="div.article_info h3", title_attr=""){
-  if(title_attr!=""){title <- html_obj %>% html_nodes(title_node_info) %>% html_attr(title_attr)}else{
-  title <- html_obj %>% html_nodes(title_node_info) %>% html_text()}
-  Encoding(title) <- "UTF-8"
-  return(title)
-}
+getContentTitle <-
+  function(html_obj,
+           title_node_info = "div.article_info h3",
+           title_attr = "") {
+    if (title_attr != "") {
+      title <-
+        html_obj %>% html_nodes(title_node_info) %>% html_attr(title_attr)
+    } else{
+      title <- html_obj %>% html_nodes(title_node_info) %>% html_text()
+    }
+    Encoding(title) <- "UTF-8"
+    return(title)
+  }
 
 
-#' Get Content datetime
+#' Get Content Datetime
 #'
 #' Get naver news published datetime from link.
 #'
@@ -135,24 +165,33 @@ getContentTitle<-function(html_obj, title_node_info="div.article_info h3", title
 #' @import xml2
 #' @import rvest
 
-getContentDatetime<-function(html_obj, datetime_node_info="span.t11", datetime_attr="", getEdittime=TRUE){
-  if(datetime_attr!=""){datetime <- html_obj %>% html_nodes(datetime_node_info) %>% html_attr(datetime_attr)}else{
-    datetime <- html_obj %>% html_nodes(datetime_node_info) %>% html_text()}
-  datetime <- as.POSIXlt(datetime)
+getContentDatetime <-
+  function(html_obj,
+           datetime_node_info = "span.t11",
+           datetime_attr = "",
+           getEdittime = TRUE) {
+    if (datetime_attr != "") {
+      datetime <-
+        html_obj %>% html_nodes(datetime_node_info) %>% html_attr(datetime_attr)
+    } else{
+      datetime <-
+        html_obj %>% html_nodes(datetime_node_info) %>% html_text()
+    }
+    datetime <- as.POSIXlt(datetime)
 
-  if(getEdittime){
-    if (length(datetime) == 1) {
-      edittime <- datetime[1]
+    if (getEdittime) {
+      if (length(datetime) == 1) {
+        edittime <- datetime[1]
+      }
+      if (length(datetime) == 2) {
+        edittime <- datetime[2]
+        datetime <- datetime[1]
+      }
+      datetime <- c(datetime, edittime)
+      return(datetime)
     }
-    if (length(datetime) == 2) {
-      edittime <- datetime[2]
-      datetime <- datetime[1]
-    }
-    datetime<-c(datetime,edittime)
     return(datetime)
   }
-  return(datetime)
-}
 
 
 #' Get Content Press name.
@@ -167,14 +206,21 @@ getContentDatetime<-function(html_obj, datetime_node_info="span.t11", datetime_a
 #' @import xml2
 #' @import rvest
 
-getContentPress<-function(html_obj, press_node_info="div.article_header div a img", press_attr="title"){
-  if(press_attr!=""){press <- html_obj %>% html_nodes(press_node_info) %>% html_attr(press_attr)}else{
-    press <- html_obj %>% html_nodes(press_node_info) %>% html_text()}
-  Encoding(press) <- "UTF-8"
-  return(press)
-}
+getContentPress <-
+  function(html_obj,
+           press_node_info = "div.article_header div a img",
+           press_attr = "title") {
+    if (press_attr != "") {
+      press <-
+        html_obj %>% html_nodes(press_node_info) %>% html_attr(press_attr)
+    } else{
+      press <- html_obj %>% html_nodes(press_node_info) %>% html_text()
+    }
+    Encoding(press) <- "UTF-8"
+    return(press)
+  }
 
-#' Get Content body name.
+#' Get Content Body.
 #'
 #' Get naver news body from link.
 #'
@@ -187,14 +233,85 @@ getContentPress<-function(html_obj, press_node_info="div.article_header div a im
 #' @import rvest
 #' @import stringi
 
-getContentBody<-function(html_obj, body_node_info="div#articleBodyContents", body_attr=""){
-  if(body_attr!=""){body <- html_obj %>% html_nodes(body_node_info) %>% html_attr(body_attr)}else{
-    body <- html_obj %>% html_nodes(body_node_info) %>% html_text()}
-  Encoding(body) <- "UTF-8"
+getContentBody <-
+  function(html_obj,
+           body_node_info = "div#articleBodyContents",
+           body_attr = "") {
+    if (body_attr != "") {
+      body <-
+        html_obj %>% html_nodes(body_node_info) %>% html_attr(body_attr)
+    } else{
+      body <- html_obj %>% html_nodes(body_node_info) %>% html_text()
+    }
+    Encoding(body) <- "UTF-8"
 
-  body <- gsub("\r?\n|\r", " ", body)
-  body <- gsub("// flash .* function _flash_removeCallback\\(\\) \\{\\} ","",body)
-  body <- stri_trim_both(body)
+    body <- gsub("\r?\n|\r", " ", body)
+    body <-
+      gsub("// flash .* function _flash_removeCallback\\(\\) \\{\\} ",
+           "",
+           body)
+    body <- stri_trim_both(body)
 
-  return(body)
+    return(body)
+  }
+
+#' Check Content Summary.
+#'
+#' Get naver news summary from link.
+#'
+#' @param html_obj "xml_document" "xml_node" using read_html function.
+#' @param summary_node_info Information about node names like tag with class or id. Default is "div.article_info h3" for naver news title.
+#' @param summary_attr if you want to get attribution text, please write down here.
+#' @return Get character summary content.
+#' @export
+#' @import rvest
+#' @import httr
+
+checkContentSummary <-
+  function(html_obj,
+           summary_node_info = "div.media_end_head_autosummary",
+           summary_attr = "") {
+    if (summary_attr != "") {
+      chk <-
+        html_obj %>% html_nodes(summary_node_info) %>% html_attr(summary_attr)
+    } else{
+      chk <- html_obj %>% html_nodes(summary_node_info) %>% html_text()
+    }
+    if (identical(chk, character(0))) {
+      chk <- F
+    } else {
+      chk <- T
+    }
+
+    return(chk)
+  }
+
+
+
+#' Get Content Summary.
+#'
+#' Get naver news summary from link.
+#'
+#' @param root is naver news link.
+#' @return Get character summary content.
+#' @export
+#' @import rvest
+#' @import httr
+
+getContentSummary <- function(root) {
+  params <- strsplit(root, "=|&")[[1]]
+  oid <- params[grep("oid", params) + 1]
+  aid <- params[grep("aid", params) + 1]
+  tar <- paste0("http://tts.news.naver.com/article/",
+                oid,
+                "/",
+                aid,
+                "/summary")
+  summ <-
+    GET(tar) %>%
+    content("parsed")
+  summ<-gsub("<br/>", " ", summ$summary)
+  summ<-gsub("  ", " ", summ$summary)
+
+  return(summ)
 }
